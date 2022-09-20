d84ab8f2ca32efcf890a558ded00bcb5136d5e7f
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2022-09-20 19:01:28 +0530

    Create find-max-n-min-array.py

---
+++ questions/find-max-n-min-array.py
@@ -0,0 +1,11 @@
+arr = [3, 2, -1, 56, 10000, 167]
+
+maximum, minimum = arr[0], arr[0]
+
+for x in arr:
+    if x < minimum:
+        minimum = x
+    if x > maximum:
+        maximum = x
+
+print(maximum, minimum)
