fea1f2812fafe8857e7d0992ef2a477435149c7f
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2022-10-09 18:43:16 +0530

    Update merge_sort_advanced.py

--- sort/merge_sort_advanced.py
+++ sort/merge_sort_advanced.py
@@ -3,6 +3,8 @@
 def merge_two(left, right, array):
     p1 = p2 = k = 0
 
+    # start a pointer and compare the arrays
+    # in order to sort them
     while p1 < len(left) and p2 < len(right):
         if left[p1] < right[p2]:
             array[k] = left[p1]
@@ -11,6 +13,10 @@ def merge_two(left, right, array):
             array[k] = right[p2]
             p2 += 1
         k += 1
+
+    # iterating once more through both arrays
+    # for just in case few elements are missed
+    # due to different array length
     while p1 < len(left):
         array[k] = left[p1]
         p1 += 1
@@ -22,16 +28,16 @@ def merge_two(left, right, array):
 
 def merge_sort(array):
     if len(array) <= 1:
-        return array
-    else:
-        mid_point = len(array) // 2 
-        left = array[:mid_point]
-        right = array[mid_point:]
+        return
 
-        merge_sort(left),
-        merge_sort(right),
-        
-        merge_two(left, right, array)
+    midpoint = len(array) // 2 
+    left_arr = array[:midpoint]
+    right_arr = array[midpoint:]
+
+    merge_sort(left_arr)
+    merge_sort(right_arr)
+
+    merge_two(left_arr, right_arr, array)
 
 # print(merge_two([2, 6], [4, 9, 10]))
 x = [2, 4, 9, 5, 2]
